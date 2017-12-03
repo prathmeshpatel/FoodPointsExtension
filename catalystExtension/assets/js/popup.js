2e76869e55185688ddc6060dee29dab0731824ac
prathmeshpatel <pjp112497@gmail.com>
2017-12-03 01:20:43 -0500

    added button for new tab

--- catalystExtension/assets/js/popup.js
+++ catalystExtension/assets/js/popup.js
@@ -1,7 +1,15 @@
 $("h1").click(function() {
     $(this).toggleClass("devil");
 });
-
+window.addEventListener('DOMContentLoaded', function() {
+    // your button here
+    var link = document.getElementById('btnOpenNewTab');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        var newURL = "https://my.duke.edu/students/dashboard#continuing";
+        chrome.tabs.create({ url: newURL });
+    });
+});
 $(".fa-arrow-circle-down").click(function() {
     $("#mealList").slideToggle("fast");
 });
