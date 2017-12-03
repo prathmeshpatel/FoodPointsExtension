d1470d2c7b6052341bcc341d8df095e2b93b7a82
Grant Kim <grantkim93@gmail.com>
2017-12-03 03:21:59 -0500

    days of week

--- catalystExtension/dropdown.js
+++ catalystExtension/dropdown.js
@@ -1,10 +1,27 @@
-// makes the calendar - sorry for mixing my js with my html
-var calendarRow = "<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>";
-for(var i = 0; i < 6; i++) {
-    $("table").append(calendarRow);
+// makes the days - sorry for mixing my js with my html
+var weekday = new Array(7);
+weekday[0] =  "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+
+for(var i = 0; i < 7; i++) {
+    var d = new Date();
+    var dayPlan = "<div class=\"dayOfWeek\">";
+    if(d.getDay() + i > 6) {
+        dayPlan += "<h1>" + weekday[d.getDay() + i - 6] + "</h1>";
+    } else {
+        dayPlan += "<h1>" + weekday[d.getDay() + i] + "</h1>";
+    }
+    dayPlan += "<div class=\"dayMeals\"></div>";
+    dayPlan += "</div>";
+    $("#days").append(dayPlan);
 }
 
-$("td").on("click", function() {
+$(".dayMeals").on("click", function() {
     var restaurant = $("#meal").val();
     var meal = $("#category").val();
     if(restaurant != null && meal != null) {
